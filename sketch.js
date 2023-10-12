d99eb30006f317a6a488061a2729315fd6f44a5f
LuisMnt <djlgkp501@gmail.com>
2023-10-12 15:55:47 -0500

    experimento con for loop

--- sketch.js
+++ sketch.js
@@ -1,18 +1,37 @@
-let miPelota;
-let frutas = ["manzana", "pera", "piña"];
-
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  miPelota = new Pelota();
+  noLoop();
 }
 
 function draw() {
-  background(0);
-  circle(mouseX, mouseY, 20);
-  miPelota.update(300);
-  miPelota.display();
+  background(220);
+  noStroke();
+  let anchura = 30;
+  let gap = 10;
+
+  for (let x = anchura; x < windowWidth - anchura; x += anchura + gap) {
+    for (let y = anchura; y < windowHeight - anchura; y += anchura + gap) {
+      fill(190);
+      square(x, y, anchura);
+    }
+  }
 }
+
+// let miPelota;
+// let frutas = ["manzana", "pera", "piña"];
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   miPelota = new Pelota();
+// }
+
+// function draw() {
+//   background(0);
+//   circle(mouseX, mouseY, 20);
+//   miPelota.update(300);
+//   miPelota.display();
+// }
